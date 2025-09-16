bcea5fdef172ab93e8f1f57b126b6bb1985aed6e
sr-roboto <marcoslopez123.lu@gmail.com>
2025-09-16 14:30:12 -0300

    commit cualquiera 2

--- src/app-cualquiera.ts
+++ src/app-cualquiera.ts
@@ -1,4 +1,4 @@
 import { ClaseCualquiera } from './clases/cualquiera.clase';
 
 const oaaa = ClaseCualquiera.metodoCualquiera('aaa', 76, true);
-oaaa.mostrar();
+oaaa.mostrarCualquiera();
--- src/interfaces/cualquiera.interface.ts
+++ src/interfaces/cualquiera.interface.ts
@@ -2,5 +2,5 @@ import { ClaseCualquiera } from '../clases/cualquiera.clase';
 
 export interface ICualquiera {
   // metodoCualquiera(a: string, b: number, c: boolean): ClaseCualquiera;
-  mostrarCualquiera(): void;
+  mostrarCualquiera(): any;
 }
